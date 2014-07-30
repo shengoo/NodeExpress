5b0248e27a4f989c039c94c9bdec7ab75f96d969
Qing Sheng <shengoo@hotmail.com>
2014-07-30 20:09:48 +0800

    modified   app.js modified   package.json modified   routes/index.js

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 var express = require('express');
+var expressSession = require('express-session');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var mongo = require('./mongo');
+var mongoUrl = 'mongodb://localhost:27017/dbname';
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// This is a middleware that we will use on routes where
+// we _require_ that a user is logged in, such as the /secret url
+function requireUser(req, res, next){
+  if (!req.user) {
+    res.redirect('/not_allowed');
+  } else {
+    next();
+  }
+}
+
+// This middleware checks if the user is logged in and sets
+// req.user and res.locals.user appropriately if so.
+function checkIfLoggedIn(req, res, next){
+  if (req.session.username) {
+    var coll = mongo.collection('users');
+    coll.findOne({username: req.session.username}, function(err, user){
+      if (user) {
+        // set a 'user' property on req
+        // so that the 'requireUser' middleware can check if the user is
+        // logged in
+        req.user = user;
+        
+        // Set a res.locals variable called 'user' so that it is available
+        // to every handlebars template.
+        res.locals.user = user;
+      }
+      
+      next();
+    });
+  } else {
+    next();
+  }
+}
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +60,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( expressSession({
+  secret: 'somesecretrandomstring'
+}));
+
+// We must use this middleware _after_ the expressSession middleware,
+// because checkIfLoggedIn checks the `req.session.username` value,
+// which will not be available until after the session middleware runs.
+app.use(checkIfLoggedIn);
 
 app.use('/', routes);
 app.use('/users', users);
@@ -61,7 +108,11 @@ var debug = require('debug')('test');
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  console.log("server start http://localhost:3000")
-  debug('Express server listening on port ' + server.address().port);
-});
+
+mongo.connect(mongoUrl, function(){
+    console.log('Connected to mongo at: ' + mongoUrl);
+    app.listen(app.get('port'), function() {
+        console.log("server start http://localhost:3000")
+        debug('Express server listening on port 3000');
+    });
+})
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  var coll = mongo.collection('users');
+  coll.find({}).toArray(function(err, users){
+    res.render('index', {users:users});  
+  })
 });
 
 router.get('/hi',function(req,res){
